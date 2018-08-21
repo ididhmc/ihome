370eecf2834b70685a26cc2edda16f9a66875545
husky <huskycreatenode@gmail.com>
2018-08-20 20:39:30 -0700

    web

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "ihome/routers"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"strings"
+	"net/http"
+)
+
+func main() {
+	ignnoreStaticPath()
+	beego.Run()
+}
+func ignnoreStaticPath(){
+	beego.InsertFilter("/",beego.BeforeRouter,TransparentStatic)
+	beego.InsertFilter("/*",beego.BeforeRouter,TransparentStatic)
+}
+func TransparentStatic(ctx *context.Context){
+	orpath:=ctx.Request.URL.Path
+	beego.Debug("request url:",orpath)
+	if strings.Index(orpath,"api")>=0{
+		return
+	}
+	beego.Info("responseWrite: ",ctx.ResponseWriter)
+	beego.Info("requset:",ctx.Request)
+	http.ServeFile(ctx.ResponseWriter,ctx.Request,"static/html/"+ctx.Request.URL.Path)
+}
